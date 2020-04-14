06ce409197a6a9ebcddf8c3c9284aa64fb1c6c24
cfbonner <cfbonner@gmail.com>
2020-04-14 23:20:59 +0100

    Sending parsed commands to server

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -14,7 +14,7 @@ defmodule KVServer.Command do
     {:ok, {:delete, "shopping", "eggs"}}
   
   ## Unkown commands or commands with the wrong number of arguments
-  return an erros
+  return an error.
 
     iex> KVServer.Command.parse("UNKOWN shopping eggs\r\n")
     {:error, :unknown_command}
@@ -29,6 +29,44 @@ defmodule KVServer.Command do
       ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
       ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
       _ -> {:error, :unknown_command}
+    end
+  end
+  
+  @doc """
+  Runs the given command.
+  """
+  def run(command)
+
+  def run({:create, bucket}) do
+    KV.Registry.create(KV.Registry, bucket)
+    {:ok, "OK\r\n"}
+  end
+
+  def run({:get, bucket, key}) do
+    lookup(bucket, fn pid ->
+      value = KV.Bucket.get(pid, key)
+      {:ok, "#{value}\r\rOK\r\n"}
+    end)
+  end
+
+  def run({:put, bucket, key, value}) do
+    lookup(bucket, fn pid ->
+      KV.Bucket.put(pid, key, value)
+      {:ok, "OK\r\n"}
+    end)
+  end
+
+  def run({:delete, bucket, key}) do
+    lookup(bucket, fn pid ->
+      value = KV.Bucket.delete(pid, key)
+      {:ok, "#{value}\r\nOK\r\n"}
+    end)
+  end
+
+  defp lookup(bucket, callback) do
+    case KV.Registry.lookup(KV.Registry, bucket) do
+      {:ok, pid} -> callback.(pid)
+      :error -> {:error, :not_found}
     end
   end
 end
